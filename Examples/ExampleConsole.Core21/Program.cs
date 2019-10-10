8aad26b672b109516cfc6d627953b908c3b6d80f
Nik <kano@pulter.tv>
2019-10-10 11:24:37 +0400

    * change example for usage CappedLog.hosting.extensions

--- Examples/ExampleConsole.Core21/Program.cs
+++ Examples/ExampleConsole.Core21/Program.cs
@@ -1,51 +1,51 @@
-﻿using CappedLog;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
 
-namespace ExampleConsole.Core21 { 
+namespace ExampleConsole.Core21
+{
     class Program
     {
         static void Main(string[] args)
         {
-            var log = new CappedLog.CappedLog();
-            var builder = new CappedLog.CappedLogConfBuilder()
-                .AddConstLabel("app", Assembly.GetAssembly(typeof(Program)).GetName().Name)
-                .SetDefaultCapacity(10);
-
-            var serviceCollection = new ServiceCollection();
-            ConfigureServices(serviceCollection, builder, log);
-
-            var cancel = new CancellationTokenSource();
-            var scrapeProcess = new CappedLog.LokiScrapeProcess(new Uri("http://localhost:3100/api/prom/push"), TimeSpan.FromSeconds(10));
-            var scrape = new CappedLog.CappedLogScrape();
-            scrape
-                .SetScrapeInterval(TimeSpan.FromSeconds(1))
-                .SetScrape(log, scrapeProcess);
-            var scrapeTask = scrape.Start(cancel.Token);
-
-            var serviceProvider = serviceCollection.BuildServiceProvider();
-
-            var logger = serviceProvider.GetService<ILogger<Program>>();
-            logger.LogError(new IndexOutOfRangeException("error message 1"), "Error {}", "1");
-            logger.LogCritical(new EventId(1, "Code 1"), new DllNotFoundException("error message 1"), "Critical {}", "2");
-            logger.LogWarning(new EventId(2), "Warning {}", "3");
-
-            Task.Delay(3000).Wait();
-            cancel.Cancel();
+            using (var host = new HostBuilder()
+                .ConfigureLogging(c => c.AddCappedLog())
+                .ConfigureServices(c => c.AddHostedService<ExampleWorker>()
+                    .AddCappedLogLokiScrape().Configure<CappedLog.LokiConfig>(o =>
+                        {
+                            o.ScrapeInterval = TimeSpan.FromSeconds(1);
+                            o.Timeout = TimeSpan.FromSeconds(10);
+                            o.Url = new Uri("http://localhost:3100/api/prom/push");
+                        }))
+                .Build())
+            {
+                host.Run();
+            }
         }
 
-        private static void ConfigureServices(IServiceCollection services, CappedLog.CappedLogConfBuilder builder, CappedLog.ICappedLogStorrage storrage)
+        class ExampleWorker : BackgroundService
         {
-            services.AddLogging(configure => configure.AddCappedLog(conf =>
+            private readonly ILogger<ExampleWorker> _logger;
+
+            public ExampleWorker(ILogger<ExampleWorker> logger) { _logger = logger; }
+
+            protected override async Task ExecuteAsync(CancellationToken stoppingToken)
             {
-                conf.DefaultBuilder = builder;
-                conf.Storrage = storrage;
-                conf.IncludeScopes = true;
-            }));
+                while (true)
+                {
+                    stoppingToken.ThrowIfCancellationRequested();
+
+                    _logger.LogError(new IndexOutOfRangeException("error message 1"), "Error {}", "1");
+                    _logger.LogCritical(new EventId(1, "Code 1"), new DllNotFoundException("error message 1"), "Critical {}", "2");
+                    _logger.LogWarning(new EventId(2), "Warning {}", "3");
+
+                    await Task.Delay(2000);
+                }
+
+            }
         }
 
     }
